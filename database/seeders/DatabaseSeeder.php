a43747b8beea6645bf23da1b97664fdead95009d
SaraNiknamHesar <saraniknaam76@gmail.com>
2025-12-07 20:08:05 +0100

    4_4_frontend_master

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Database\Seeders\Admin\AdminSeeder;
+use Database\Seeders\Frontend\UserSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,6 +23,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AdminSeeder::class);
+        // $this->call(AdminSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
